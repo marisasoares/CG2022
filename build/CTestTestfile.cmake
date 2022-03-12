2f3265d00db95375c84d4bef6612dc247da10145
Miguel Rodrigues Santa Cruz <62035620+MiguelSantaCruz@users.noreply.github.com>
2022-03-12 00:21:59 +0000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miguel/Documentos/Universidade/3º ano/2º Semestre/Computação gráfica/CG2022
-# Build directory: /home/miguel/Documentos/Universidade/3º ano/2º Semestre/Computação gráfica/CG2022/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
